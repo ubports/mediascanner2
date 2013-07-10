1fc0e98be21a6f87d26a73ba8e03e6c0aa48a8bc
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-07-10 14:10:57 +0300

    Started on subtree change watcher.

---
+++ subtreewatcher.cc
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * Authors:
+ *    Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include<cstdio>
+
+int main(int argc, char **argv) {
+    if(argc != 2) {
+        printf("%s <subdir to watch>\n", argv[0]);
+        return 1;
+    }
+    return 0;
+}
