eee21f86f9e91906eb5d1516e44994c82f675491
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-09-03 14:21:06 +0300

    Return dummy data.

--- scanscope.cpp
+++ scanscope.cpp
@@ -24,9 +24,50 @@
 #include<unity.h>
 
 static void search_func(UnityScopeSearchBase* search, void* user_data) {
+    GSList *results = NULL;
+    GHashTable *metadata = NULL;
+    UnityScopeResult scope_result = { 0, };
+
+    /* Iterate through the returned results and add them to the
+     * Unity's result set
+     */
+    for (int i=0; i<1; i++) {
+
+        /* Build and populate a scope result from the source data */
+        scope_result.uri = "file:///home/jpakkane/Music/KLF - Justified And Ancient.mp3";
+        scope_result.title = "Justified and Ancient";
+        //scope_result.icon_hint = result->icon_url;
+        scope_result.category = 0;
+        scope_result.result_type = UNITY_RESULT_TYPE_DEFAULT;
+        scope_result.mimetype = "audio/mp3";
+        //scope_result.comment = result->description;
+        //scope_result.dnd_uri = result->link;
+
+        /* Insert the metadata, if available */
+        /*
+        metadata = g_hash_table_new(g_str_hash, g_str_equal);
+        g_hash_table_insert(metadata, "author",
+                g_variant_new_string((const gchar*)"KLF"));
+         if (result->creation_date) {
+             g_hash_table_insert(metadata, "creation_date",
+                                 g_variant_new_string(result->creation_date));
+         }
+         */
+        scope_result.metadata = metadata;
+
+         /*
+          * Add the returned result to the search results list, taking a
+          * copy of the data passed in via scope_result
+          */
+        unity_result_set_add_result(search->search_context->result_set,
+                                     &scope_result);
+        g_hash_table_unref(metadata);
+     }
+
 }
 
 static UnityAbstractPreview* preview_func(UnityResultPreviewer *previewer, void *user_data) {
+    return nullptr;
 }
 
 int main(void) {
