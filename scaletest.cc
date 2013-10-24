0ec2f87e08058b24a543ef8cbe362586bb31f31f
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-10-24 14:41:28 +0300

    Can query stuff.

--- scaletest.cc
+++ scaletest.cc
@@ -19,19 +19,47 @@
 
 #include"MediaStore.hh"
 #include"MediaFile.hh"
+#include "utils.hh"
 
 #include<vector>
 #include<string>
 #include<fstream>
 #include<cstdio>
 #include<random>
+#include<sqlite3.h>
 
 using namespace std;
 
 #define RNDWORD words[rnd() % words.size()]
 
-int main() {
-    MediaStore store("scaletest");
+int printer(void*/*arg*/, int num_cols, char **data, char **colnames) {
+    for(int i=0; i<num_cols; i++) {
+        printf("%s: %s\n", colnames[i], data[i]);
+    }
+    return 0;
+}
+
+int queryDb(const string &fname, const string &core_term) {
+    sqlite3 *db;
+    const char *music_templ = "SELECT * FROM music WHERE rowid IN (SELECT docid FROM music_fts WHERE artist MATCH %s UNION SELECT docid FROM music_fts WHERE title MATCH %s);";
+    string term = sqlQuote(core_term + "*");
+    if(sqlite3_open_v2(fname.c_str(), &db, SQLITE_OPEN_READONLY, nullptr) != SQLITE_OK) {
+        printf("%s\n", sqlite3_errmsg(db));
+        return 1;
+    }
+    char cmd[1024];
+    sprintf(cmd, music_templ, term.c_str(), term.c_str());
+    char *err;
+    printf("Music results\n\n");
+    if(sqlite3_exec(db, cmd, printer, NULL, &err) != SQLITE_OK) {
+        printf("%s\n", sqlite3_errmsg(db));
+        return 1;
+    }
+    return 0;
+}
+
+int createDb(const string &base) {
+    MediaStore store(base);
     ifstream ifile("/usr/share/dict/words");
     string line;
     vector<string> words;
@@ -63,4 +91,15 @@ int main() {
         }
     }
     return 0;
+}
+
+int main(int argc, char **argv) {
+    if(argc == 1) {
+        return createDb("scaletest");
+    } else if(argc == 2) {
+        return queryDb("scaletest-mediastore.db", argv[1]);
+    } else {
+        printf("Read the source to understand.\n");
+        return 1;
+    }
 }
