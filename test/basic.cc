582c252df38ac8d678c2db890dec1c51c5674a4d
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2013-10-25 14:06:27 +0300

    Added a test.

---
+++ test/basic.cc
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * Authors:
+ *    Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include<MediaStore.hh>
+#include<cassert>
+#include<cstdio>
+#include<string>
+#include<unistd.h>
+
+using namespace std;
+
+void init_test() {
+    string base("basic");
+    string fname = base + "-mediastore.db";
+    unlink(fname.c_str());
+    MediaStore store(base);
+}
+
+int main() {
+#ifdef NDEBUG
+    fprintf(stderr, "NDEBUG defined, tests won't work.\n");
+    return 1;
+#else
+    init_test();
+    return 0;
+#endif
+}
