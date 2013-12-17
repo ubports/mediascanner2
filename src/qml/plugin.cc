4a2b4d3352731c7f0a369551daa95e208f72f2f2
James Henstridge <james@jamesh.id.au>
2013-12-17 14:09:02 +0800

    Update packaging to cover QML plugin, and set plugin version to 0.1.

--- src/qml/plugin.cc
+++ src/qml/plugin.cc
@@ -3,7 +3,7 @@
 #include "MediaStoreWrapper.hh"
 
 void MediaScannerPlugin::registerTypes(const char *uri) {
-    qmlRegisterType<MediaStoreWrapper>(uri, 1, 0, "MediaStore");
-    qmlRegisterUncreatableType<MediaFileWrapper>(uri, 1, 0, "MediaFile",
+    qmlRegisterType<MediaStoreWrapper>(uri, 0, 1, "MediaStore");
+    qmlRegisterUncreatableType<MediaFileWrapper>(uri, 0, 1, "MediaFile",
         "Use a MediaStore to retrieve MediaFiles");
 }
