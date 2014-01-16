c683253ce5c28ae64165265e09c68a712f802ef0
James Henstridge <james@jamesh.id.au>
2014-01-16 23:11:53 +0800

    Add LGPL headers to QML plugin code.

--- src/qml/Ubuntu/MediaScanner/MediaStoreWrapper.hh
+++ src/qml/Ubuntu/MediaScanner/MediaStoreWrapper.hh
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2013 Canonical, Ltd.
+ *
+ * Authors:
+ *    James Henstridge <james.henstridge@canonical.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef MEDIASCANNER_QML_MEDIASTOREWRAPPER_H
 #define MEDIASCANNER_QML_MEDIASTOREWRAPPER_H
 
