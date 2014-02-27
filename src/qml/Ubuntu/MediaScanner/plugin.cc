970cc572b71e0081ff22ba88f4105fe3b654a2a0
James Henstridge <james@jamesh.id.au>
2014-02-27 13:37:36 +0800

    Add songs listing model.

--- src/qml/Ubuntu/MediaScanner/plugin.cc
+++ src/qml/Ubuntu/MediaScanner/plugin.cc
@@ -20,6 +20,7 @@
 #include "plugin.hh"
 #include "MediaFileWrapper.hh"
 #include "MediaStoreWrapper.hh"
+#include "SongsModel.hh"
 #include "SongsSearchModel.hh"
 
 using namespace mediascanner::qml;
@@ -28,5 +29,6 @@ void MediaScannerPlugin::registerTypes(const char *uri) {
     qmlRegisterType<MediaStoreWrapper>(uri, 0, 1, "MediaStore");
     qmlRegisterUncreatableType<MediaFileWrapper>(uri, 0, 1, "MediaFile",
         "Use a MediaStore to retrieve MediaFiles");
+    qmlRegisterType<SongsModel>(uri, 0, 1, "SongsModel");
     qmlRegisterType<SongsSearchModel>(uri, 0, 1, "SongsSearchModel");
 }
