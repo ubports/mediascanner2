9af8f578ba899f6723008d40e7d2939f5b0001d0
Jussi Pakkanen <jussi.pakkanen@canonical.com>
2014-03-11 10:23:03 +0200

    Skip subdirectories that look like top level root directories.

---
+++ src/daemon/util.cc
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2014 Canonical, Ltd.
+ *
+ * Authors:
+ *    Jussi Pakkanen <jussi.pakkanen@canonical.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "util.h"
+
+#include<sys/stat.h>
+
+using namespace std;
+
+static bool dir_exists(const string &path) {
+    struct stat statbuf;
+    stat(path.c_str(), &statbuf);
+    return S_ISDIR(statbuf.st_mode) ;
+}
+
+bool is_rootlike(const string &path) {
+    string s1 = path + "/usr";
+    string s2 = path + "/var";
+    string s3 = path + "/bin";
+    return dir_exists(s1) && dir_exists(s2) && dir_exists(s3);
+}
