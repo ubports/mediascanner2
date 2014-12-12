1ea923f2625b41ffc0f5a950bad0031f6c95fd06
James Henstridge <james@jamesh.id.au>
2014-12-12 18:27:12 +0800

    Add copyright headers.

--- src/extractor/main.cc
+++ src/extractor/main.cc
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2014 Canonical, Ltd.
+ *
+ * Authors:
+ *    James Henstridge <james.henstridge@canonical.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <cstdio>
 #include <memory>
 #include <stdexcept>
